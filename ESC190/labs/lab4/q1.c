5aeb722ab0deff9a51f9f19d85e30e704ec7f199
Adam Omarali <omarali3@remote.ecf.utoronto.ca>
2024-02-05 16:14:01 -0500

    lab4

--- ESC190/labs/lab4/q1.c
+++ ESC190/labs/lab4/q1.c
@@ -5,4 +5,12 @@ int main()
     int a = 43;
     int *p_a = a;
     printf("%ld", *p_a);
+
+    FILE *fp = fopen("test.txt", "r");
+    // if (fp == NULL)
+    // {
+    //     pritnf("No file\n");
+    // }
+
+    fclose(fp);
 }
